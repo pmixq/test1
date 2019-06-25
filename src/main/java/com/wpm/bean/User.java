480fce50ead0275a17303f903c2f00ae742fc060
wpm <389668362@qq.com>
2019-06-25 21:07:39 +0800

    from idea commit

---
+++ src/main/java/com/wpm/bean/User.java
@@ -0,0 +1,38 @@
+package com.wpm.bean;
+
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
